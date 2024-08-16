3d7200180dc6db2dab7f6f9f45baa5e362010e94
Pavel Peremyshlev <pavel.peremyshlev@axenix.pro>
2024-08-17 00:10:30 +0300

    Models for user, song and artist

---
+++ internal/database/models.go
@@ -0,0 +1,40 @@
+package database
+
+import (
+	"github.com/jinzhu/gorm"
+)
+
+type User struct {
+	gorm.Model
+	Name          string
+	Email         string
+	PasswordHash  string
+	Role          string
+	UploadedSongs []Song `gorm:"foreignKey:UploadedBy"`
+}
+
+type Artist struct {
+	gorm.Model
+	Name        string
+	Description string
+	ImageUrl    string
+	Songs       []SongArtist
+}
+
+type Song struct {
+	gorm.Model
+	Title       string
+	Description string
+	Content     string
+	Artists     []SongArtist
+	UploadedBy  uint
+}
+
+type SongArtist struct {
+	gorm.Model
+	ArtistID   uint
+	Artist     Artist
+	SongID     uint
+	Song       Song
+	TitleOrder int
+}
