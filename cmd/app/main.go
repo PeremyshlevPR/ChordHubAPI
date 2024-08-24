28e1e5f5fdf01c290777992e9421367a3851bade
Pavel Peremyshlev <pavel.peremyshlev@axenix.pro>
2024-08-24 13:22:30 +0300

    auth middleware ready

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -1,14 +1,12 @@
 package main
 
 import (
+	"chords_app/internal/config"
+	"chords_app/internal/database"
 	"chords_app/internal/repositories"
 	"chords_app/internal/services"
 	"chords_app/internal/web"
 	"chords_app/internal/web/handlers"
-	"net/http"
-
-	"chords_app/internal/config"
-	"chords_app/internal/database"
 	"log/slog"
 )
 
@@ -33,6 +31,9 @@ func main() {
 	userHandler := handlers.NewUserHandler(userService, &cfg.Roles)
 
 	router := web.SetupRouter(userHandler)
+
 	slog.Info("Starting HTTP server", "host", cfg.Server.Host, "port", cfg.Server.Port)
-	http.ListenAndServe(cfg.Server.Host+":"+cfg.Server.Port, router)
+	if err := router.Run(cfg.Server.Host + ":" + cfg.Server.Port); err != nil {
+		slog.Error("error starting server:", slog.String("error", err.Error()))
+	}
 }
